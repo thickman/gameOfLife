d4d13a44eb95aad069a65aae76edfb3d1e55ab87
thickman <adam@thickman.local>
2018-11-27 16:52:23 +0100

    DEV: bug fixing, simplifying: SquareComponent created based on dataBoard from now on

--- src/components/BoardComponent.js
+++ src/components/BoardComponent.js
@@ -7,12 +7,15 @@ export default class BoardComponent extends React.Component {
     super(props);
 
     this.state = {
+      speed: 3000, //ms
       dimension: 6,
       viewBoard: [],
       dataBoard: [],
     }
 
     this.initializeBoard();
+
+
   }
 
   onSquareChanged(x, y){
@@ -32,11 +35,17 @@ export default class BoardComponent extends React.Component {
     let j=0;
 
     for(let k=0; k<dimension*dimension; k++) {
-      makeAlive = i==3 && j==2;
+      makeAlive = i==3 && j==2 ||
+        i==4 && j==2 ||
+        i==5 && j==2;
+
+
+      /*
       arr.push(
+        // TODO: usunac to - ta linia tylko przy render, tutaj tworzyc tylko dataBoard
         <SquareComponent key={'sqr_'+k} x={i} y={j} isAlive={makeAlive} onSquareChanged={this.onStateChanged}/>
       )
-
+*/
       dataArr[k] = makeAlive ? true : false;
 
       i++;
@@ -51,14 +60,17 @@ export default class BoardComponent extends React.Component {
       viewBoard: arr,
       dataBoard: dataArr
     }
+
+
   }
 
   get2d(k){
-    const ij = k < this.state.dimension - 1
+    const ij = k < this.state.dimension
       ? {i: k , j: 0}
       : {
-        i: k % this.state.dimension, j: Math.ceil(k / 3)
+        i: k % this.state.dimension, j: Math.floor(k / this.state.dimension)
       }
+      return ij;
   }
 
   get1d(i, j){
@@ -69,16 +81,11 @@ export default class BoardComponent extends React.Component {
     let viewBoard = this.state.viewBoard;
     let dataBoard = this.state.dataBoard;
     let width = this.state.dimension;
-
-
-
     let hasUp, hasDown, hasLeft, hasRight;
-
     const k = this.get1d(i, j);
 
-    console.log("k: "+k);
-
-    console.log("dataBoardOfMe: "+ dataBoard[k]);
+    //console.log("k: "+k);
+    //console.log("dataBoardOfMe: "+ dataBoard[k]);
 
     hasUp    = j > 0;
     hasDown  = j < width - 1;
@@ -86,8 +93,6 @@ export default class BoardComponent extends React.Component {
     hasRight = i < width - 1;
 
     const neighbours = [];
-
-
 
     neighbours[0] = hasUp && hasLeft
       ? dataBoard[k - width - 1]
@@ -122,7 +127,42 @@ export default class BoardComponent extends React.Component {
       : false;
 
     return neighbours.filter(neighbour => (!!neighbour)).length
+  }
 
+
+  printIndexes(array){
+    const newDataBoardPrint = array.reduce((printableDataBoard, current, index) => {
+      if(current){
+        printableDataBoard.push(index);
+      }
+      return printableDataBoard;
+    }, [])
+
+
+    return newDataBoardPrint;
+  }
+
+
+  calculateNewState(arg){
+
+    const newDataBoard = arg.state.dataBoard.map((isAlive, index) => {
+      const xy = arg.get2d(index);
+      const neighbourAmount = arg.liveNeighboursOf(xy.i, xy.j);
+
+      const makeAlive =
+        (neighbourAmount === 3 && !isAlive) ||
+        ((neighbourAmount === 2 || neighbourAmount == 3) && isAlive);
+
+      return makeAlive;
+    })
+
+
+    const oldDataBoardPrint = arg.printIndexes(arg.state.dataBoard);
+    const newDataBoardPrint = arg.printIndexes(newDataBoard);
+    console.log("oldDataBoardPrint: "+oldDataBoardPrint);
+    console.log("newDataBoardPrint: "+newDataBoardPrint);
+    //console.log("newDataBoard: "+newDataBoard);
+    return newDataBoard;
   }
 
   onChangeState(x, y) {
@@ -130,7 +170,6 @@ export default class BoardComponent extends React.Component {
   }
 
   render(){
-    //const dimension = this.props.dimension;
     //const speed = this.props.speed;
 
     const fillColour = 'yellow';
@@ -143,17 +182,27 @@ export default class BoardComponent extends React.Component {
 
     console.log("dataBoard: "+ this.state.dataBoard);
 
+    //const one = this.liveNeighboursOf(1,3);
+    //const two = this.liveNeighboursOf(3,2);
+    //const three = this.liveNeighboursOf(2,2);
 
-    const one = this.liveNeighboursOf(1,3);
-    const two = this.liveNeighboursOf(3,2);
-    const three = this.liveNeighboursOf(2,2);
+    //console.log("one: "+one);
+    //console.log("two: "+two);
+    //console.log("three: "+three);
 
-    console.log("one: "+one);
-    console.log("two: "+two);
-    console.log("three: "+three);
+    setTimeout(this.calculateNewState, this.state.speed, this);
 
     return (<svg>
-      {this.state.viewBoard}
+      {
+        this.state.dataBoard.map((square, index) => {
+          //console.log("index: "+index);
+          const xy = this.get2d(index);
+          //console.log("xy: "+JSON.stringify(xy));
+          return (
+            <SquareComponent key={'sqr_'+index} x={xy.i} y={xy.j} isAlive={!!square}/>
+          )
+        })
+      }
       </svg>
     );
     }
